fae04e7d1a08573fd63477edae0d7d321902ef32
Esther ZR <esther.zigterman.rustenburg@voys.nl>
2023-04-17 15:28:11 +0200

    Update collect_data.py

--- wherescape/connectors/hubspot/collect_data.py
+++ wherescape/connectors/hubspot/collect_data.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 from wherescape import WhereScape
+from hubspot_wrapper import Hubspot
 
 # def wherescape_instance():
 #     start_time = datetime.now()
@@ -26,10 +27,3 @@ def hubspot_load_data():
     result = wherescape_instance.query_target(sql)
     if len(result) > 0:
         logging.info(result[0])
-
-
-# def hubspot_load_fact_ps_user():
-
-#     ori_sql = "SELECT fu.client_id, fu.user_amount, fu.user_addition, fu.user_subtraction, COALESCE(fu.user_addition, 0) + COALESCE(fu.user_subtraction, 0) AS user_change, dhcv.hubspot_company_id FROM star.fact_ps_user fu INNER JOIN star.dim_hubspot_company_voys dhcv ON dhcv.voipgrid_pk = fu.client_id WHERE fu.'date'= CURRENT_DATE ORDER BY user_amount DESC"
-#     wherescape_instance = WhereScape()
-#     result = wherescape_instance.query_target(ori_sql)
