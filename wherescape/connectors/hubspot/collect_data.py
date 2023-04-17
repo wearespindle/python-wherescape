157b09fd12cf853b868d7bcfd6cf1a7b48e73825
Esther ZR <esther.zigterman.rustenburg@voys.nl>
2023-04-17 15:48:23 +0200

    formatting updated in hope of fix

--- wherescape/connectors/hubspot/collect_data.py
+++ wherescape/connectors/hubspot/collect_data.py
@@ -2,15 +2,17 @@ import logging
 from datetime import datetime
 from wherescape import WhereScape
 
+
 def hubspot_load_data():
     start_time = datetime.now()
     logging.info("connecting to WhereScape")
     wherescape_instance = WhereScape()
     logging.info(
-        "Start time: %s for hubspot_load_data" % start_time.strftime("%Y-%m-%d %H:%M:%S")
+        "Start time: %s for hubspot_load_data"
+        % start_time.strftime("%Y-%m-%d %H:%M:%S")
     )
     logging.info("post load")
-    table_name = wherescape_instance.load_full_name()
+    table_name = wherescape_instance.load_full_name
     logging.info(table_name)
     sql = f"select * from {table_name}"
     result = wherescape_instance.query_target(sql)
