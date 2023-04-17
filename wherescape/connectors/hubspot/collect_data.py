4a7ae3215ec354529b54817319d4a6c33cf86fb7
Esther ZR <esther.zigterman.rustenburg@voys.nl>
2023-04-17 15:43:03 +0200

    Update collect_data.py

--- wherescape/connectors/hubspot/collect_data.py
+++ wherescape/connectors/hubspot/collect_data.py
@@ -1,17 +1,6 @@
 import logging
 from datetime import datetime
 from wherescape import WhereScape
-# from wherescape.connectors.hubspot.hubspot_wrapper import Hubspot
-
-# def wherescape_instance():
-#     start_time = datetime.now()
-#     logging.info("connecting to WhereScape")
-#     wherescape_instance = WhereScape()
-#     logging.info(
-#         "Start time: %s for hubspot_load_data" % start_time.strftime("%Y-%m-%d %H:%M:%S")
-#     )
-#     logging.info("post load")
-#     return wherescape_instance
 
 def hubspot_load_data():
     start_time = datetime.now()
