783b631996f5537b0adf5d5210222e2564568036
Esther ZR <esther.zigterman.rustenburg@voys.nl>
2023-05-01 16:23:28 +0200

    now column names are compared to property names

--- wherescape/connectors/hubspot/process_data.py
+++ wherescape/connectors/hubspot/process_data.py
@@ -1,6 +1,8 @@
 import logging
 from .hubspot_wrapper import Hubspot
 
+# from helper_functions import compare_names
+
 """
 This module processes the collected data so it can be send to the Hubspot Module
 """
@@ -11,16 +13,16 @@ This module processes the collected data so it can be send to the Hubspot Module
 
 def hubspot_process_results(api_key: str, results, column_names: list):
     """
-    method to process results to Hubspot
+    function to process results to Hubspot
     """
     hubspot_instance = Hubspot(api_key)
     properties = []
     property_names = hubspot_instance.get_company_properties()
-    compare_propery_column_names(column_names, property_names)
+    known_names = compare_names(column_names, property_names)
     for result in results:
-        # Hubspot only accepts 100 items at a time
+        """Hubspot only accepts 100 items per call"""
         if len(properties) < 100:
-            properties.append(string_to_dict(result, column_names))
+            properties.append(create_data_dict(result, column_names, known_names))
         else:
             """
             send the collected data in patch, empty properties and start with the next results
@@ -28,7 +30,7 @@ def hubspot_process_results(api_key: str, results, column_names: list):
             logging.info("full batch ready")
             send_data("patch", "companie", properties, hubspot_instance)
             properties.clear()
-            properties.append(string_to_dict(result, column_names))
+            properties.append(create_data_dict(result, column_names, known_names))
 
     if len(properties) > 0:
         logging.info("final batch ready")
@@ -36,26 +38,28 @@ def hubspot_process_results(api_key: str, results, column_names: list):
 
 
 def send_data(
-    object_type: str, change_type: str, properties, hubspot_instance: Hubspot
+    object_type: str, change_type: str, properties: list, hubspot_instance: Hubspot
 ):
     """
     Method to send data in the correct direction for
     object_type (company, contact, deals) and
     change_type (patch)
     """
-    if object_type.lower() == "companies" or object_type.lower() == "company":
-        if change_type.lower() == "patch":
-            logging.info("company patch")
+    if object_type == "companies":
+        if change_type == "patch":
+            logging.info("companies patch")
             hubspot_instance.send_company_patch(inputs=properties)
-    if object_type.lower() == "contacts" or object_type.lower() == "contact":
-        if change_type.lower() == "patch":
+    if object_type == "contacts":
+        if change_type == "patch":
+            logging.info("contacts patch")
             hubspot_instance.send_contact_patch(inputs=properties)
-    if object_type.lower() == "deals" or object_type.lower() == "deals":
-        if change_type.lower() == "patch":
+    if object_type == "deals":
+        if change_type == "patch":
+            logging.info("deals patch")
             hubspot_instance.send_deal_patch(inputs=properties)
 
 
-def string_to_dict(result, column_names):
+def create_data_dict(result, column_names: list, known_names: list):
     """
     Method to process a result list to a dict of keys id and properties.
     All elements besides hubspot_company_id are stored in a dict under properties
@@ -64,42 +68,69 @@ def string_to_dict(result, column_names):
     result_dict = {}
     property_dict = {}
 
-    # TODO: dit kan vast beter!!!
     for name in column_names:
-        if name == "hubspot_company_id":
-            result_dict["id"] = result[column_names.index(name)]
-        elif name == "user_amount":
-            property_dict["users"] = result[column_names.index(name)]
-        # elif name == "user_change":
-        #     property_dict["daily_user_change"] = result[column_names.index(name)]
-        elif name == "user_addition" and ("user_subtraction" in column_names):
-            if (
-                type(result[column_names.index(name)]) == None
-                and result[column_names.index("user_subtraction")] == None
-            ) or (
-                type(result[column_names.index(name)]) != None
-                and result[column_names.index("user_subtraction")] == None
-            ):
-                property_dict["daily_user_change"] = result[column_names.index(name)]
-            elif (
-                type(result[column_names.index(name)]) == None
-                and result[column_names.index("user_subtraction")] != None
-            ):
-                property_dict["daily_user_change"] = result[column_names.index(name)]
+        if name in known_names:
+            """
+            For 1-to-1 column_names and property_names
+            """
+            if name == "id":
+                result_dict[name] = result[column_names.index(name)]
             else:
-                property_dict["daily_user_change"] = (
-                    result[column_names.index(name)]
-                    + result[column_names.index("user_subtraction")]
-                )
+                property_dict["users"] = result[column_names.index(name)]
+        else:
+            # TODO: find more abstract way of doing this
+            if name == "user_addition" and ("user_subtraction" in column_names):
+                addition = result[column_names.index(name)]
+                subtraction = result[column_names.index("user_subtraction")]
+
+                if addition != None and subtraction != None:
+                    property_dict["daily_user_change"] = addition + subtraction
+                else:
+                    property_dict["daily_user_change"] = (
+                        subtraction if subtraction != None else addition
+                    )
 
     result_dict.update({"properties": property_dict})
 
     return result_dict
 
 
-def compare_propery_column_names(column_names: list, property_names: list):
-    for name in column_names:
-        if name not in property_names:
-            logging.warning(
-                "Column name: %s does not exist as a property with the same name" % name
-            )
+def compare_names(source_names: list, destination_names: list):
+    """
+    This function compares source names with destiny names for one to one data transfer.
+    It will give a warning for source names that do not appear in the list of destiny names
+    """
+    known_destination_names = []
+    for name in source_names:
+        if name not in destination_names:
+            logging.warning("source name: %s does not exist in the dest" % name)
+        else:
+            known_destination_names.append(name)
+
+    return known_destination_names
+
+
+def get_object_name(table_name: str):
+    if "companies" in table_name:
+        return "companies"
+    elif "contacts" in table_name:
+        return "contacts"
+    elif "deals" in table_name:
+        return "deals"
+    elif "tickets" in table_name:
+        return "tickets"
+    else:
+        raise Exception("Could not find the specific hubspot object type in table name")
+
+
+def get_http_request_name(table_name: str):
+    if "patch" in table_name:
+        return "patch"
+    elif "post" in table_name:
+        return "post"
+    elif "get" in table_name:
+        return "get"
+    elif "delete" in table_name:
+        return "delete"
+    else:
+        raise Exception("Coild not find the specific http request in table name")
